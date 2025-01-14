0e5f5c53252fb89095e2f1f6a223cfcf17fbe3b6
Abraham Bhatti <abrahambhatti@icloud.com>
2025-01-13 17:37:11 -0800

    skills changes

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,3 +1,4 @@
+import React, { useState, useRef, useEffect } from 'react';
 import "./Homepage.css";
 import happyGilmore from './assets/happygilmore.jpeg';
 import hobbyImage from './assets/guitar.jpeg';
@@ -5,6 +6,48 @@ import artistImage from './assets/justice.jpeg';
 import profilepic from './assets/profile.jpg';
 
 function Homepage() {
+  const [selectedSkill, setSelectedSkill] = useState(null);
+  const popupRef = useRef(null);
+
+  const handleClick = (skill) => {
+    setSelectedSkill(skill);
+  };
+
+  const closePopup = () => {
+    setSelectedSkill(null);
+  };
+
+  useEffect(() => {
+    if (selectedSkill) {
+      const handleClickOutside = (event) => {
+        if (popupRef.current && !popupRef.current.contains(event.target)) {
+          closePopup();
+        }
+      };
+
+      document.addEventListener('mousedown', handleClickOutside);
+
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }
+  }, [selectedSkill]);
+
+  const skillDescriptions = {
+    Java: "I first learned Java in my high school AP Computer Science class. As I grasped the basics, I created new projects that helped me develop a much stronger understanding of the language.",
+    Python: "Python was the first programming language I learned. I practiced it during my sophomore year of high school, utilizing object-oriented programming to create games and simulations. I continue to improve my Python skills through independent coursework and projects.",
+    HTML: "HTML/CSS was new to me until this year when I started learning it over winter break to develop my personal website.",
+    "C/C++": "C/C++ is part of my Computer Science and Engineering curriculum. I continue to learn and practice new concepts weekly to assist me in future project development.",
+    FrontEnd: "Though Front-End develop is a new interest of mine, very much enjoy the creative freedom and new opportunities that come with web development.",
+    BackEnd: "Back-End development has been my main focus for many years, as I have learned how to implement various programming languages and logic to suit my goals.",
+    Leadership: "I have held multiple leadership roles in clubs and jobs, but I learned the most from my experience as a Taekwondo instructor.",
+    Organization: "My experience in high-pressure work environments has helped me develop strong organizational skills in intense situations.",
+    Communication: "As a high school athlete, I learned how to be part of a team and communicate more effectively with others, gaining a deeper understanding of the importance of collaboration in the workplace.",
+    ProblemSolving: "I have always enjoyed solving complex problems and using critical thinking to find efficient solutions. Attending hackathons allows me to further pursue this interest under high stakes and strict deadlines.",
+    TimeManagement: "I learned the importance of practice and time management from my years as a musician. Though this required much dedication and patience, I learned how to balance different aspects of my life and manage my time better."
+  };
+  
+
   return (
     <div className="home">
       <header className="header">
@@ -14,34 +57,47 @@ function Homepage() {
           <p><strong>Computer Science & Engineering @ Santa Clara University</strong></p>
         </div>
       </header>
+
       <section className="about">
         <h2>About Me</h2>
         <p><strong>I am an aspiring software engineer at SCU, focused on deepening my technological expertise. I seek valuable internship experience to apply my knowledge in the engineering world. I am passionate about learning from diverse perspectives, expanding my horizons through real-world projects, and connecting with fellow innovators.</strong></p>
       </section>
+
       <section className="all-skills">
         <section className="skills-section">
           <h2>Technical Skills/Interests</h2>
           <div className="skills-container">
-            <button className="skill-button">Java</button>
-            <button className="skill-button">C/C++</button>
-            <button className="skill-button">HTML/CSS</button>
-            <button className="skill-button">Python</button>
-            <button className="skill-button">Front End</button>
-            <button className="skill-button">Back End</button>
-            </div>
-            </section>
-            
-            <section className="other-skills-section">
-              <h2>Other Skills</h2>
-              <div className="other-skills-container">
-                <button className="other-skill-button">Leadership</button>
-                <button className="other-skill-button">Organization</button>
-                <button className="other-skill-button">Communication</button>
-                <button className="other-skill-button">Problem-Solving/Critical</button>
-                <button className="other-skill-button">Time Management</button>
-                </div>
-                </section>
+            <button className="skill-button" onClick={() => handleClick("C/C++")}>C/C++</button>
+            <button className="skill-button" onClick={() => handleClick("Python")}>Python</button>
+            <button className="skill-button" onClick={() => handleClick("Java")}>Java</button>
+            <button className="skill-button" onClick={() => handleClick("HTML")}>HTML/CSS</button>
+            <button className="skill-button" onClick={() => handleClick("BackEnd")}>Back End</button>
+            <button className="skill-button" onClick={() => handleClick("FrontEnd")}>Front End</button>
+          </div>
         </section>
+
+        <section className="other-skills-section">
+          <h2>Other Skills</h2>
+          <div className="other-skills-container">
+            <button className="other-skill-button" onClick={() => handleClick("Leadership")}>Leadership</button>
+            <button className="other-skill-button" onClick={() => handleClick("Organization")}>Organization</button>
+            <button className="other-skill-button" onClick={() => handleClick("Communication")}>Communication</button>
+            <button className="other-skill-button" onClick={() => handleClick("ProblemSolving")}>Problem-Solving</button>
+            <button className="other-skill-button" onClick={() => handleClick("TimeManagement")}>Time Management</button>
+          </div>
+        </section>
+
+        {selectedSkill && (
+          <div className="popup-overlay">
+            <div className="popup" ref={popupRef}> {/* Attach ref to the popup */}
+              <h3>{selectedSkill}</h3>
+              <p>{skillDescriptions[selectedSkill]}</p>
+              <button onClick={closePopup}>Close</button>
+            </div>
+          </div>
+        )}
+      </section>
+
       <h3>Favorites:</h3>
       <section className="favorite-section">
         <div className="favorite">
@@ -78,6 +134,7 @@ function Homepage() {
           </div>
         </div>
       </section>
+
       <footer className="footer">
       </footer>
     </div>
